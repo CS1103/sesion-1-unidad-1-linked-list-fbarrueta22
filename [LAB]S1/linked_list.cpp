cd9bee53171ec0f132a6a017d9343c82b77e0382
fbarrueta22 <frings-22@hotmail.com>
2020-04-11 23:30:09 -0500

    linked list

--- [LAB]S1/linked_list.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by fbarrueta on 11/04/20.
-//
-
-#include "linked_list.h"
--- [LAB]S1/linked_list.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by fbarrueta on 11/04/20.
-//
-
-#ifndef _LAB_S1_LINKED_LIST_H
-#define _LAB_S1_LINKED_LIST_H
-
-#endif //_LAB_S1_LINKED_LIST_H
